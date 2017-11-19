9e0dd9ea31e355d73d680edfb5313e9ac5f080ef
Leon de Pruyssenaere de la Woestijne <leonpw@outlook.com>
2017-11-19 12:08:52 +0200

    Add project files.

---
+++ CompositePriceCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Workshop.DPtoCT.HandsOn02
+{
+    public class CompositePriceCalculator : IPriceCalculator
+    {
+        public CompositePriceCalculator(params IPriceCalculator[] calculators)
+        {
+        }
+
+        public decimal CalculatePrice(Basket basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
